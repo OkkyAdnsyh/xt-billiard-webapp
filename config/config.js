ed73cdfcc1715860fee6ea94b0ed08a8ac18ffa3
OkkyAdnsyh <landing.pageportofolio@gmail.com>
2024-06-10 19:56:48 +0700

    db(config):db connection set up

---
+++ config/config.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+module.exports = {
+    development: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    },
+    test: {
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    },
+    production: {
+      username: process.env.PROD_DB_USERNAME,
+      password: process.env.PROD_DB_PASSWORD,
+      database: process.env.PROD_DB_NAME,
+      host: process.env.PROD_DB_HOSTNAME,
+      port: process.env.PROD_DB_PORT,
+      dialect: 'mysql',
+    },
+  };
