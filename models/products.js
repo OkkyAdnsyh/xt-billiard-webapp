191063be2461fcfef74b899a11001ae6e83bcc20
OkkyAdnsyh <landing.pageportofolio@gmail.com>
2024-06-13 20:13:32 +0700

    package:uninstall sequelize, sequelize-cli, mysql2

--- models/products.js
+++ models/products.js
@@ -1,32 +1,35 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const mongoose = require('mongoose');
 
-const Products = sequelize.define('Product', {
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    table_type: {
-        type: DataTypes.ENUM,
-        values: ['reguler', 'vip'],
-        allowNull: false
-    },
-    table_status: {
-        type: DataTypes.ENUM,
-        values: ['occupied', 'vacant'],
-        allowNull: false,
-        defaultValue: 'vacant'
-    },
-    hourly_price: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    coach_price: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
-}, {timestamps: true});
+const Products = mongoose.Schema
+
+// const Products = sequelize.define('Product', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     table_type: {
+//         type: DataTypes.ENUM,
+//         values: ['reguler', 'vip'],
+//         allowNull: false
+//     },
+//     table_status: {
+//         type: DataTypes.ENUM,
+//         values: ['occupied', 'vacant'],
+//         allowNull: false,
+//         defaultValue: 'vacant'
+//     },
+//     hourly_price: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//     },
+//     coach_price: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     }
+// }, {timestamps: true});
 
 module.exports = Products;
